cecd3bd2c1c83d393183c4b82ce136bbd83b82c0
Lalit Das <daslalitmohan@gmail.com>
2025-10-28 18:57:40 +0530

    Change key_name from 'Netflix' to 'us-east1'

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0bbdd8c17ed981ef9"
   instance_type          = "t2.medium"
-  key_name               = "Netflix"
+  key_name               = "us-east1"
   vpc_security_group_ids = ["sg-0bef669f582095a6a"]
   tags = {
     Name = var.instance_names[count.index]
